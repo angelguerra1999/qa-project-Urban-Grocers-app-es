2dcb5b5af7ac7612bf86bb9eee1ba499bc9217c9
angelguerra1999 <134436845+angelguerra1999@users.noreply.github.com>
2024-12-28 18:50:13 -0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = # Almacena la ruta para crear un kit en esta variable
+URL_SERVICE = "https://cnt-9b1d173e-91cf-47e4-a542-ebfae54dd7df.containerhub.tripleten-services.com"
+CREATE_USER_PATH = "/api/v1/users/"
+CREATE_KIT_PATH = "/api/v1/kits/"
