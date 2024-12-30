2e51b88932bcdf6f04bdd9ebb276843255965d38
angelguerra1999 <134436845+angelguerra1999@users.noreply.github.com>
2024-12-30 15:26:29 -0300

    Update create_kit_name_kit_test.py

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -1,76 +1,71 @@
 import sender_stand_request
 import data
 
-def get_kit_body(name):
-    kit_body = {"name": name}
-    return kit_body
+def get_kit_body(kit_name):
+    current_body = data.kit_body.copy()
+    current_body['name'] = kit_name
+    return current_body
 
-def positive_assert(kit_body, auth_token):
-    response = sender_stand_request.post_new_client_kit(kit_body, auth_token)
-    assert response.status_code == 201
-    assert response.json()["name"] == kit_body["name"]
+def get_new_user_autoken():
+    new_user = sender_stand_request.post_new_user(data.user_body.copy())
+    au_token_value = new_user.json()["authToken"]
+    new_user_autoken = data.headers.copy()
+    new_user_autoken["Authorization"] = f"Bearer {au_token_value}"
+    return new_user_autoken
 
-def negative_assert_code_400(kit_body, auth_token):
-    response = sender_stand_request.post_new_client_kit(kit_body, auth_token)
-    assert response.status_code == 400
+def positive_assert(kit_name):
+    kit_body = get_kit_body(kit_name)
+    kit_response = sender_stand_request.post_new_kit(kit_body, get_new_user_autoken())
+    assert kit_response.status_code == 201
+    assert kit_response.json()["name"] == kit_name
 
-def get_new_user_token():
-    user_body = data.user_body
-    user_response = sender_stand_request.post_new_user(user_body)
-    return user_response.json()["authToken"]
+def negative_assert_symbol(kit_name):
+    # El cuerpo actualizado de la solicitud se guarda en la variable user_body
+    kit_body = get_kit_body(kit_name)
+    # El resultado se guarda en la variable response
+    kit_response = sender_stand_request.post_new_kit(kit_body, get_new_user_autoken())
+    # Comprueba si el código de estado es 400
+    assert kit_response.status_code == 400
+    # Comprueba que el atributo code en el cuerpo de respuesta es 400
+    assert kit_response.json()["code"] == 400
 
-def test_create_kit_with_1_letter_name():
-    auth_token = get_new_user_token()
-    kit_body = get_kit_body("a")
-    positive_assert(kit_body, auth_token)
+# Prueba 1. Kit creado con éxito. El parámetro name contiene 1 caracteres
+def test_create_kit_1_letter_in_name_get_success_response():
+    positive_assert("a")
 
-def test_create_kit_with_511_letter_name():
-    auth_token = get_new_user_token()
-    kit_body = get_kit_body("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC")
-    positive_assert(kit_body, auth_token)
+#prueba 2. Kit creado con éxito. El parámetro name contiene 511 caracteres
+def test_create_kit_511_letters_in_name_get_success_response():
+    positive_assert("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC")
 
-def test_create_kit_with_empty_name():
-    auth_token = get_new_user_token()
-    kit_body = get_kit_body("")
-    negative_assert_code_400(kit_body, auth_token)
+#Prueba 3. Error. El parámetro contiene un string vacío
+def test_create_kit_empty_name_get_error_response():
+    negative_assert_symbol("")
 
-def test_create_kit_with_512_letter_name():
-    auth_token = get_new_user_token()
-    kit_body = get_kit_body("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD")
-    negative_assert_code_400(kit_body, auth_token)
+# Prueba 4. Error. El parámetro name contiene 512 caracteres
+def test_create_kit_512_letters_in_name_get_error_response():
+        negative_assert_symbol("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD")
 
-def test_create_kit_with_special_characters():
-    auth_token = get_new_user_token()
-    kit_body = get_kit_body("№%@")
-    positive_assert(kit_body, auth_token)
+#Prueba 5. El parámetro name contiene un string de caracteres especiales
+def test_create_kit_special_caracaters_get_success_response():
+    positive_assert("\"№%@\",")
 
-def test_create_kit_with_spaces():
-    auth_token = get_new_user_token()
-    kit_body = get_kit_body(" A Aaa ")
-    positive_assert(kit_body, auth_token)
+# Prueba 6. El parámetro name contiene espacios
+def test_create_kit_white_space_get_success_response():
+    positive_assert(" A Aaa ")
 
-def test_create_kit_with_numbers():
-    auth_token = get_new_user_token()
-    kit_body = get_kit_body("123")
-    positive_assert(kit_body, auth_token)
+# Prueba 7. El parámetro name contiene un string de dígitos
+def test_create_kit_numbers_allowed_get_success_response():
+    positive_assert("123")
 
-def test_create_kit_without_name():
-    auth_token = get_new_user_token()
-    kit_body = {}
-    negative_assert_code_400(kit_body, auth_token)
+# Prueba 8. Error. Falta el parametro name en la solicitud
+def negative_assert_no_name_error_response():
+    # El diccionario con el cuerpo de la solicitud se copia del archivo "data" a la variable "user_body"
+    kit_body = data.kit_body.copy()
+    # El parámetro "name" se elimina de la solicitud
+    kit_body.pop("name")
+    # Comprueba la respuesta
+    negative_assert_symbol(kit_body)
 
-def test_create_kit_with_number_name():
-    auth_token = get_new_user_token()
-    kit_body = get_kit_body(123)
-    negative_assert_code_400(kit_body, auth_token)
-
-if __name__ == "__main__":
-    test_create_kit_with_1_letter_name()
-    test_create_kit_with_511_letter_name()
-    test_create_kit_with_empty_name()
-    test_create_kit_with_512_letter_name()
-    test_create_kit_with_special_characters()
-    test_create_kit_with_spaces()
-    test_create_kit_with_numbers()
-    test_create_kit_without_name()
-    test_create_kit_with_number_name()
+# Prueba 9. Error. El tipo del parámetro name: número
+def test_create_kit_number_type_get_error_response():
+    negative_assert_symbol(123)
